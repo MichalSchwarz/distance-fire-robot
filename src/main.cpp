ec573bf9c2c5a9a27f4ff9e35674fa87b2c84bb9
Michal Schwarz <bc.michal.schwarz@gmail.com>
2017-01-08 13:51:44 +0100

    Pridani zpetne vazby k zataceni + ucesani kodu pro prvni release

--- src/main.cpp
+++ src/main.cpp
@@ -1,270 +1,198 @@
-#include <Arduino.h>
-#include <AccelStepper.h>
-#include <Servo.h>
-
-#define HALFSTEP 8
-#define FULLSTEP 4
-#define blue 2
-#define pink 3
-#define yellow 4
-#define orange 5
-#define MAX_SPEED 200
-#define ACCELERATION 100
-AccelStepper stepper1(HALFSTEP, blue, yellow, pink, orange);
-
-#define SERVO_PIN 10
-Servo servo;
-unsigned long servo_timer = 0;
-unsigned int servo_angle = 90;
-unsigned int desired_servo_angle = 90;
-unsigned int servo_speed = 100;
-
-#define S_DONE 0
-#define S_SCANNING 1
-#define S_SET_DIRECTION 2
-#define S_MOVE 3
-unsigned int actual_step = S_SCANNING;
-
-bool is_scan_running = false;
-float distances[181];
-#define S_SCAN_START_POSITION 1
-#define S_SCAN_SCANNING 2
-#define S_SCAN_FINAL_POSITION 3
-unsigned int actual_scanning_step = S_SCAN_START_POSITION;
-
-bool is_direction_running = false;
-#define S_DIRECTION_STEERING 1
-#define S_DIRECTION_MOVING 2
-#define S_DIRECTION_FINAL_POSITION 3
-unsigned int actual_direction_step = S_DIRECTION_STEERING;
-
-#define ECHOPIN 7
-#define TRIGPIN 8
-#define ECHO_ENABLE_PIN 9
-float closest_distance = 5000;
-float closest_distance_base = 0;
-unsigned int closest_angle = 90;
-
-
 /**
-* Nastavi vychozi hodnoty rychlosti a akcelerace stepperu
-*/
-void init_stepper(unsigned int speed, unsigned int acceleration);
+ * Ovladani robota, ktery najde nejblizsi objekt v rozsahu -90 az +90 stupnu
+ * od primeho smeru robota a dojede k nemu.
+ *
+ * @author Michal Schwarz <bc.michal.schwarz@gmail.com>
+ * @licence MIT
+ */
 
-/**
-* Pootoci stepperem o pocet otacek po smeru hod. rucicek.
-* Zaporne cislo = opacny smer
-*/
-void move_stepper_rotations(float rotations);
-
-void init_echo();
-
-void move_stepper_cm(float cm);
-
-void servo_run(unsigned int angle, unsigned int speed);
-
-void set_servo_angle(unsigned int angle);
-
-unsigned int get_servo_angle();
-unsigned int get_desired_servo_angle();
-
-void run_scanning();
-
-void run_set_direction();
-
-bool is_scanning();
-
-float get_distance_cm();
-
-float get_rotations_by_angle(unsigned int angle);
-
-bool prase = true;
-bool koza = false;
+#include "main.h"
 
 void setup() {
   init_stepper(MAX_SPEED, ACCELERATION);
   init_echo();
   servo.attach(SERVO_PIN);
-  Serial.begin(9600);
-  //move_stepper_cm(20);
 }
 
 void loop() {
-
   if(actual_step == S_SCANNING)
   {
     run_scanning();
-    actual_step = is_scanning() ? S_SCANNING : S_SET_DIRECTION;
   }
   else if(actual_step == S_SET_DIRECTION)
   {
     run_set_direction();
-    actual_step = is_direction_running ? S_SET_DIRECTION : S_MOVE;
   }
   else if(actual_step == S_MOVE)
   {
-
-
+    run_final_move();
   }
-
-
-  stepper1.run();
+  stepper.run();
   servo_run(desired_servo_angle, servo_speed);
+}
+
+void run_final_move()
+{
+  float distance_cm = get_distance_cm();
+  if(distance_cm > TOUCH_DISTANCE_CM)
+  {
+    move_stepper_cm(distance_cm - TOUCH_DISTANCE_CM);
+  }
+  actual_step = S_DONE;
+}
+
+float get_steering_step(int angle, float actual_distance)
+{
+  float step = ZERO_ROTATIONS;
+  bool tolerance = angle < DIRECT_ANGLE + DIRECT_ANGLE_TOLERANCE &&
+                   angle > DIRECT_ANGLE - DIRECT_ANGLE_TOLERANCE;
+  if(actual_distance < TOUCH_DISTANCE_CM || tolerance)
+  {
+    step = ZERO_ROTATIONS;
+  }
+  else
+  {
+    if(actual_distance < ROTATION_CM + TOUCH_DISTANCE_CM)
+    {
+      step = (actual_distance - TOUCH_DISTANCE_CM) / ROTATION_CM;
+    }
+    else
+    {
+      step = DEFAULT_STEERING_ROTATIONS;
+    }
+  }
+  return step;
+}
+
+unsigned int add_angle(unsigned int angle, int add)
+{
+  int result = angle + add;
+  if(result > MAX_ANGLE)
+  {
+    result = MAX_ANGLE;
+  }
+  else if (result < MIN_ANGLE)
+  {
+    result = MIN_ANGLE;
+  }
+  return (unsigned int) result;
+}
+
+void set_scaning_range(int angle)
+{
+  if(angle > DIRECT_ANGLE)
+  {
+    scan_range_stop = add_angle(angle, STEERING_SCAN_ANGLE_LOW);
+    scan_range_start = add_angle(angle, -STEERING_SCAN_ANGLE_HIGH);
+  }
+  else
+  {
+    scan_range_start = add_angle(angle, -STEERING_SCAN_ANGLE_LOW);
+    scan_range_stop = add_angle(angle, STEERING_SCAN_ANGLE_HIGH);
+  }
 }
 
 void run_set_direction()
 {
-  bool stop_direction = false;
-  if(actual_direction_step == S_DIRECTION_STEERING)
-  {
-    if(get_desired_servo_angle() != 0)
-    {
-      set_servo_angle(0);
-    }
-    if(get_desired_servo_angle() == get_servo_angle())
-    {
-      actual_direction_step = S_DIRECTION_MOVING;
-    }
-  }
   if(actual_direction_step == S_DIRECTION_MOVING)
   {
-    Serial.print("prase\n");
-    actual_direction_step = S_DIRECTION_FINAL_POSITION;
-    move_stepper_cm(get_rotations_by_angle(closest_angle));
-
-  }
-  if(actual_direction_step == S_DIRECTION_FINAL_POSITION && !stepper1.isRunning())
-  {
-    servo_speed = 20;
-    set_servo_angle(90);
-    if(get_desired_servo_angle() == get_servo_angle())
+    float step = get_steering_step(closest_angle, closest_distance);
+    if(step == ZERO_ROTATIONS)
     {
-      move_stepper_cm(closest_distance_base-5);
-      stop_direction = true;
+      set_servo_angle(closest_angle);
+      actual_step = S_MOVE;
     }
-
+    else
+    {
+      move_stepper_rotations(step);
+      actual_direction_step = S_DIRECTION_STEERING;
+    }
   }
-  is_direction_running = !stop_direction;
-}
-
-float get_rotations_by_angle(unsigned int angle)
-{
-
-  double sensor_distance = 7.5;
-  double rotation_diameter = 14.2;
-  double diagonal_distance = sensor_distance + closest_distance;
-  double axial_distance = cos(angle / (180 / M_PI)) * diagonal_distance;
-  double radial_distance = sin(angle / (180 / M_PI)) * diagonal_distance;
-
-  double rotation_angle = 180 / M_PI * atan(axial_distance / (rotation_diameter + radial_distance));
-  float rotations_to_move = rotation_angle / 90 * 22.3;
-  //rotations_to_move = 26;
-  Serial.print(cos(angle / (180 / M_PI)));
-  Serial.print("cos\n");
-  Serial.print(rotation_angle);
-  Serial.print("x\n");
-  Serial.print(rotations_to_move);
-  Serial.print("y\n");
-  closest_distance_base = sqrt((rotation_diameter + radial_distance)*(rotation_diameter + radial_distance) + axial_distance*axial_distance)-rotation_diameter - sensor_distance;
-  Serial.print(closest_distance_base);
-  Serial.print("closest_distance_base\n");
-  /*
-  if(angle < 90)
+  if(actual_direction_step == S_DIRECTION_STEERING)
   {
-    rotations_to_move = 90 - angle;
+    if(!stepper.distanceToGo())
+    {
+      set_scaning_range(closest_angle);
+      actual_scanning_step = S_SCAN_START_POSITION;
+      actual_step = S_SCANNING;
+    }
   }
-  else if(angle > 90)
-  {
-    rotations_to_move = -(angle - 90);
-  }
-  */
-  return rotations_to_move;
 }
 
 void run_scanning()
 {
-  bool stop_scan = false;
   if(actual_scanning_step == S_SCAN_START_POSITION)
   {
-    servo_speed = 20;
-    if(get_desired_servo_angle() != 0)
+    servo_speed = SERVO_SPEED_MOVE;
+    if(get_desired_servo_angle() != scan_range_start)
     {
-      set_servo_angle(0);
+      set_servo_angle(scan_range_start);
     }
-    if(get_servo_angle() == 0)
+    if(get_servo_angle() == scan_range_start)
     {
       actual_scanning_step = S_SCAN_SCANNING;
+      closest_distance = UNREACHABLE_DISTANCE_CM;
     }
   }
 
   else if (actual_scanning_step == S_SCAN_SCANNING)
   {
-    servo_speed = 130;
+    servo_speed = SERVO_SPEED_SCAN;
     if(get_desired_servo_angle() == get_servo_angle())
     {
       distances[get_servo_angle()] = get_distance_cm();
-      unsigned int new_angle = get_servo_angle()+1;
+      unsigned int new_angle = get_servo_angle() + 1;
       set_servo_angle(new_angle);
     }
-    if(get_servo_angle() >= 180)
+    if(get_servo_angle() >= scan_range_stop)
     {
-      for(int i = 0; i < 181; i++)
+      for(unsigned int i = MIN_ANGLE; i < MAX_ANGLE + 1; i++)
       {
-        if(distances[i] < closest_distance)
+        if(distances[i] > 0 && distances[i] < closest_distance)
         {
           closest_distance = distances[i];
           closest_angle = i;
         }
       }
-      Serial.print(closest_distance);
-      Serial.print("a\n");
-      Serial.print(closest_angle);
-      Serial.print("b\n");
+      for(unsigned int i = MIN_ANGLE; i < MAX_ANGLE + 1; i++)
+      {
+        distances[i] = UNREACHABLE_DISTANCE_CM;
+      }
       actual_scanning_step = S_SCAN_FINAL_POSITION;
     }
   }
   else if (actual_scanning_step == S_SCAN_FINAL_POSITION)
   {
-    servo_speed = 20;
-    if(get_servo_angle() != 90)
+    servo_speed = SERVO_SPEED_MOVE;
+    if(get_servo_angle() != closest_angle)
     {
-      set_servo_angle(90);
+      set_servo_angle(closest_angle);
     }
     else
     {
-      stop_scan = true;
+      actual_direction_step = S_DIRECTION_MOVING;
+      actual_step = S_SET_DIRECTION;
     }
   }
-  is_scan_running = !stop_scan;
 }
 
 float get_distance_cm()
 {
-
-    digitalWrite(TRIGPIN, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIGPIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIGPIN, LOW);
-    float distance = pulseIn(ECHOPIN, HIGH) * 0.017315f;
-    delay(50);
-
-  Serial.print(distance);
-  Serial.print(" distance\n");
-
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(ECHO_DELAY_MEAS);
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(ECHO_DELAY_RESPONSE);
+  digitalWrite(TRIGPIN, LOW);
+  float distance = pulseIn(ECHOPIN, HIGH) * ECHO_DISTANCE_RATIO;
+  delay(ECHO_DELAY);
   return distance;
-}
-
-bool is_scanning()
-{
-  return is_scan_running;
 }
 
 void init_stepper(unsigned int speed, unsigned int acceleration)
 {
-  stepper1.setSpeed(speed);
-  stepper1.setAcceleration(acceleration);
-  stepper1.setMaxSpeed(2000.0);
+  stepper.setSpeed(speed);
+  stepper.setAcceleration(acceleration);
+  stepper.setMaxSpeed(MAX_SPEED);
 }
 
 void init_echo()
@@ -277,13 +205,13 @@ void init_echo()
 
 void move_stepper_rotations(float rotations)
 {
-  int stepper_steps = round(rotations * 4096);
-  stepper1.move(stepper_steps);
+  int stepper_steps = round(rotations * ROTATION_STEPS);
+  stepper.move(stepper_steps);
 }
 
 void move_stepper_cm(float cm)
 {
-  float rotations = cm / 16.5;
+  float rotations = cm / ROTATION_CM;
   move_stepper_rotations(rotations);
 }
 
@@ -301,7 +229,6 @@ unsigned int get_desired_servo_angle()
 {
   return desired_servo_angle;
 }
-
 
 void servo_run(unsigned int angle, unsigned int speed)
 {
---
+++ src/main.h
@@ -0,0 +1,193 @@
+/**
+ * Ovladani robota, ktery najde nejblizsi objekt v rozsahu -90 az +90 stupnu
+ * od primeho smeru robota a dojede k nemu.
+ *
+ * @author Michal Schwarz <bc.michal.schwarz@gmail.com>
+ * @licence MIT
+ */
+
+#include <Arduino.h>
+#include <AccelStepper.h>
+#include <Servo.h>
+
+#define HALFSTEP 8
+#define blue 2
+#define pink 3
+#define yellow 4
+#define orange 5
+#define MAX_SPEED 200
+#define ACCELERATION 100
+#define ROTATION_CM 16.5
+#define ROTATION_STEPS 4096
+#define TOUCH_DISTANCE_CM 10
+#define UNREACHABLE_DISTANCE_CM 1000000
+#define DEFAULT_STEERING_ROTATIONS 1
+#define ZERO_ROTATIONS 0
+AccelStepper stepper(HALFSTEP, blue, yellow, pink, orange);
+
+#define SERVO_PIN 10
+#define SERVO_SPEED_SCAN 130
+#define SERVO_SPEED_MOVE 20
+/**
+ * Timer kvuli ovladani rychlosti otaceni serva
+ */
+unsigned long servo_timer = 0;
+/**
+ * Cim mensi hodnota, tim rychlejsi pohyb. Jde o reset timeru
+ */
+unsigned int servo_speed = 100;
+unsigned int servo_angle = 90;
+unsigned int desired_servo_angle = 90;
+Servo servo;
+
+#define S_DONE 0
+#define S_SCANNING 1
+#define S_SET_DIRECTION 2
+#define S_MOVE 3
+/**
+ * Aktualne bezici smycka
+ */
+unsigned int actual_step = S_SCANNING;
+
+#define S_SCAN_START_POSITION 1
+#define S_SCAN_SCANNING 2
+#define S_SCAN_FINAL_POSITION 3
+#define MIN_ANGLE 0
+#define MAX_ANGLE 180
+#define DIRECT_ANGLE 90
+#define DIRECT_ANGLE_TOLERANCE 2
+#define STEERING_SCAN_ANGLE_LOW 10
+#define STEERING_SCAN_ANGLE_HIGH 80
+/**
+ * [uhel] => vzdalenost
+ */
+float distances[181];
+/**
+ * Aktualne bezici smycka mereni vzdalenosti
+ */
+unsigned int actual_scanning_step = S_SCAN_START_POSITION;
+unsigned int scan_range_start = MIN_ANGLE;
+unsigned int scan_range_stop = MAX_ANGLE;
+
+#define S_DIRECTION_STEERING 1
+#define S_DIRECTION_MOVING 2
+/**
+ * Aktualne bezici smycka zataceni
+ */
+unsigned int actual_direction_step = S_DIRECTION_MOVING;
+
+#define ECHOPIN 7
+#define TRIGPIN 8
+#define ECHO_ENABLE_PIN 9
+#define ECHO_DISTANCE_RATIO 0.017315f
+#define ECHO_DELAY_MEAS 2
+#define ECHO_DELAY_RESPONSE 10
+#define ECHO_DELAY 50
+float closest_distance = UNREACHABLE_DISTANCE_CM;
+unsigned int closest_angle = DIRECT_ANGLE;
+
+// *** Smycky
+// pro PHPkare je to docela orisek. Vsude globalni promenne a uplne
+// se nedari SRP. Bude treba refactoring
+
+/**
+ * Meri vzdalenost okolnich objektu
+ * @global closest_distance
+ * @global closest_angle
+ * @global actual_scanning_step
+ * @global actual_direction_step
+ * @global scan_range_stop
+ * @global scan_range_start
+ * @global actual_step
+ * @global servo_speed
+ * @global distances
+ */
+void run_scanning();
+
+/**
+ * Pootoci robotem blize k neblizsimu objektu
+ * @global closest_angle
+ * @global closest_distance
+ * @global actual_scanning_step
+ * @global actual_direction_step
+ * @global actual_step
+ */
+void run_set_direction();
+
+/**
+ * Robot je uz nasmerovan k neblizsimu objektu a dojede k nemu
+ */
+void run_final_move();
+
+/**
+ * Nastavi uhel serva danou rychlosti
+ * @global servo_timer
+ * @global servo_angle
+ */
+void servo_run(unsigned int angle, unsigned int speed);
+
+
+// *** Inicializace
+
+/**
+ * Nastavi vychozi hodnoty rychlosti a akcelerace stepperu
+ */
+void init_stepper(unsigned int speed, unsigned int acceleration);
+
+/**
+ * Inicializace mereni vzdalenosti
+ */
+void init_echo();
+
+// *** Funkce
+
+/**
+ * Pootoci stepperem o pocet otacek po smeru hod. rucicek.
+ * Zaporne cislo = opacny smer
+ */
+void move_stepper_rotations(float rotations);
+
+/**
+ * Pootoci krokovym motorem, aby robot ujel vzdalenost v cm
+ */
+void move_stepper_cm(float cm);
+
+/**
+ * Nastavi uhel serva
+ * @global desired_servo_angle
+ */
+void set_servo_angle(unsigned int angle);
+
+/**
+ * Vrati aktualni uhel serva
+ * @global servo_angle
+ */
+unsigned int get_servo_angle();
+
+/**
+ * Vrati pozadovany cilovy uhel serva
+ * @global desired_servo_angle
+ */
+unsigned int get_desired_servo_angle();
+
+/**
+ * Vrati vzdalenost objektu od senzoru v cm
+ */
+float get_distance_cm();
+
+/**
+ * Vrati o kolik otacek se ma pootocit stepper pro zatoceni
+ */
+float get_steering_step(int angle, float actual_distance);
+
+/**
+ * Nastavi rozpeti kde se bude merit vzdalenost
+ * @global scan_range_stop
+ * @global scan_range_start
+ */
+void set_scaning_range(int angle);
+
+/**
+ * Logika pro pricitani a odecitani uhlu
+ */
+unsigned int add_angle(unsigned int angle, int add);
